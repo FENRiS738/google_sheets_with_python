3cc498f13bcff0ae04d4d1bab2e4caa7c85390c1
FENRiS738 <as420738@gmail.com>
2024-04-01 15:20:33 +0530

    preetified my application

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -3,6 +3,7 @@ from googleapiclient.errors import HttpError
 from app.connection import connect
 from app.schema import User
 
+
 # To extract data from Google Sheets
 def get_data():
     creds, SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME = connect()
@@ -15,7 +16,7 @@ def get_data():
             .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
             .execute()
         )
-        
+
         values = result.get("values", [])
 
         if not values:
@@ -25,25 +26,26 @@ def get_data():
     except HttpError as err:
         return err
 
+
 # To append data in Google Sheets
 def insert_data(user):
     creds, SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME = connect()
     try:
         service = build("sheets", "v4", credentials=creds)
         sheet = service.spreadsheets()
-        
-        body = {
-            'values' : [[user.first_name, user.last_name, user.age]]
-        }
+
+        body = {"values": [[user.first_name, user.last_name, user.age]]}
 
         result = (
             sheet.values()
-            .append(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME, valueInputOption = 'USER_ENTERED', body=body)
+            .append(
+                spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                range=SAMPLE_RANGE_NAME,
+                valueInputOption="USER_ENTERED",
+                body=body,
+            )
             .execute()
         )
-        return({
-            'message': 'Record inserted successfully',
-            'user': user
-        })
+        return {"message": "Record inserted successfully", "user": user}
     except HttpError as err:
         return err
--- app/main.py
+++ app/main.py
@@ -4,17 +4,19 @@ from app.schema import User
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 def server():
     return "Server is running!"
 
 
-@app.get('/get_data_from_sheet')
+@app.get("/get_data_from_sheet")
 def get_data_from_sheet():
     values = helper_functions.get_data()
     return values
 
-@app.post('/insert_data_in_sheet')
-def insert_data_in_sheet(user: User): 
+
+@app.post("/insert_data_in_sheet")
+def insert_data_in_sheet(user: User):
     res = helper_functions.insert_data(user)
     return res
